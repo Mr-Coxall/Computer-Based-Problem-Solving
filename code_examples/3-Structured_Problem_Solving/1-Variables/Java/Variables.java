0f97173376523b55b72fe7f92ae1fd60f2393c73
Mr. Coxall <mr.coxall@mths.ca>
2020-05-28 18:35:09 +0000

    variables code

---
+++ code_examples/3-Structured_Problem_Solving/1-Variables/Java/Variables.java
@@ -0,0 +1,18 @@
+/**
+ * Class Variables.
+ * Created by: Mr. Coxall
+ * Created on: June 2020
+ * This program shows declaring variables
+ */
+public class Variables {
+  /**
+   * This is the main method.
+   */
+  public static void main(String[] args) {
+    // variable definition
+    boolean isCurrent = true;
+    int age = 32;
+    float area;
+    String someWords = "Hello, World!";
+  }
+}
