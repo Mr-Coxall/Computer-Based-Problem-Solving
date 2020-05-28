5d8962af3aa8cc577aa2d841b861fa5501ecc25c
Mr. Coxall <mr.coxall@mths.ca>
2020-05-28 16:28:42 +0000

    change go language vaariables

---
+++ code_examples/3-Structured_Problem_Solving/1-Variables/Go/variables.go
@@ -0,0 +1,12 @@
+// Created by: Mr. Coxall
+// Created on: June 2020
+// This program shows declaring variables
+
+package main
+	
+func main() {
+	var isCurrent = true // bool
+    var age = 32 // int
+    var area float32
+    var someWords = "Hello, World!" // string
+}
