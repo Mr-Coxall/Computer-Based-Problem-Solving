2eedd052eeb1d73b73fe151079f4a1dcfa923b89
Mr. Coxall <mr.coxall@mths.ca>
2020-05-29 18:34:37 +0000

    swift version

--- code_examples/3-Structured_Problem_Solving/1-Variables/JavaScript/variables.js
+++ code_examples/3-Structured_Problem_Solving/1-Variables/JavaScript/variables.js
@@ -3,6 +3,7 @@
  * Created on: June 2020
  * This program shows declaring variables
  */
+
 var isCurrent = true;
 var age = 32;
 var someWords = "Hello, World!";
---
+++ code_examples/3-Structured_Problem_Solving/1-Variables/Swift/Variables.swift
@@ -0,0 +1,11 @@
+/**
+ * Class Variables.
+ * Created by: Mr. Coxall
+ * Created on: June 2020
+ * This program shows declaring variables
+ */
+
+var isCurrent: Bool = true
+var age: Int = 32
+var area: Float = 32.45
+var someWords: String = "Hello, World!"
