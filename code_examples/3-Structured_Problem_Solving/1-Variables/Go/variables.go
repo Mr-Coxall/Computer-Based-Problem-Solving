9841afdcd4459ba341462788aa1169b4f9264a39
Mr. Coxall <mr.coxall@mths.ca>
2021-05-05 09:43:38 -0400

    lint check

--- code_examples/3-Structured_Problem_Solving/1-Variables/Go/variables.go
+++ code_examples/3-Structured_Problem_Solving/1-Variables/Go/variables.go
@@ -1,12 +1,21 @@
 // Created by: Mr. Coxall
-// Created on: June 2020
+// Created on: Sep 2020
 // This program shows declaring variables
 
 package main
+
+import "fmt"
 	
 func main() {
 	var isCurrent = true // bool
-    var age = 32 // int
-    var area float32
-    var someWords = "Hello, World!" // string
+  var age = 32 // int
+  var area float32 = 14.65 // float
+  var someWords = "Hello, World!" // string
+
+  fmt.Println(isCurrent)
+  fmt.Println(age)
+  fmt.Println(area)
+  fmt.Println(someWords)
+
+  fmt.Println("\nDone.")
 }
