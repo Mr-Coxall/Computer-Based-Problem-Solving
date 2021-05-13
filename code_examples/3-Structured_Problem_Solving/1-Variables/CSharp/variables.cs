356fd3ce0dd48e34803db51e54958d0931823687
Mr. Coxall <mr.coxall@mths.ca>
2021-05-13 11:12:33 -0400

    C#

---
+++ code_examples/3-Structured_Problem_Solving/1-Variables/CSharp/variables.cs
@@ -0,0 +1,25 @@
+/* Created by: Mr. Coxall
+ * Created on: Sep 2020
+ * This program shows declaring variables
+*/
+
+using System;
+
+/*
+ * The main class
+ * Contains all methods for performing basic variable usage
+*/
+class MainClass {
+  public static void Main (string[] args) {
+    // variable definition
+    bool isCurrent = true;               // bool
+    int age = 32;                        // int
+    float area = 32.45F;                 // float
+    string someWords = "Hello, World!";  // string
+      
+    Console.WriteLine (isCurrent);
+    Console.WriteLine (age);
+    Console.WriteLine (area);
+    Console.WriteLine (someWords);
+  }
+}
