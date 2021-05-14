2f6c89a53c96bc6c2f6f535af7caaf3dd19567ad
Mr. Coxall <mr.coxall@mths.ca>
2021-05-14 09:55:18 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,12 +33,12 @@ release = "1.0.1"
 extensions = ["sphinx_tabs.tabs"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,12 +47,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -63,4 +63,4 @@ html_logo = "images/cs-logo.png"
 
 
 def setup(app):
-    app.add_stylesheet('my_theme.css')
+    app.add_stylesheet("my_theme.css")
