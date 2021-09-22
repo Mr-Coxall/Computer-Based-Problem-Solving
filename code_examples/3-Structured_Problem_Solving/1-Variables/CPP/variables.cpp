046de26f72e29a03d96e32b4936280f6474adefe
Patrick Coxall <mr.coxall@mths.ca>
2021-09-21 22:15:56 -0400

    Update variables.cpp

--- code_examples/3-Structured_Problem_Solving/1-Variables/CPP/variables.cpp
+++ code_examples/3-Structured_Problem_Solving/1-Variables/CPP/variables.cpp
@@ -4,12 +4,17 @@
 // Created on: Sep 2020
 // This program shows declaring variables
 
-#include <stdio.h>
+#include <iostream>
 
 int main() {
     // variable definition
-    bool isCurrent = true;
-    int age = 32;
-    float area;
-    string someWords = "Hello, World!";
+    bool isCurrent = true;  // bool
+    int age = 32;  // int
+    float area = 32.45;  // float
+    std::string someWords = "Hello, World!";  // string
+
+    std::cout << isCurrent << std::endl;
+    std::cout << age << std::endl;
+    std::cout << area << std::endl;
+    std::cout << someWords << std::endl;
 }
