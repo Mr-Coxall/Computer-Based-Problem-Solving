e83167875d932057d652966acaa6af575a645ee1
Patrick Coxall <mr.coxall@mths.ca>
2021-09-21 22:29:52 -0400

    Update variables.rb

--- code_examples/3-Structured_Problem_Solving/1-Variables/Ruby/variables.rb
+++ code_examples/3-Structured_Problem_Solving/1-Variables/Ruby/variables.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 #
 # Created by: Mr. Coxall
-# Created on: June 2020
+# Created on: Sept 2020
 # This program shows declaring variables
 
-is_current = true
-age = 32
-area = 32.45
-some_words = "Hello, World!"
+is_current = true  # bool
+age = 32  # int
+area = 32.45  # float
+some_words = "Hello, World!"  # string
+
+puts is_current
+puts age
+puts area
+puts some_words
