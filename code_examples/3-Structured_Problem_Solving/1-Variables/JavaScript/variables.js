e737b6626d212f4970e35daf1c0487adecb29b22
Patrick Coxall <mr.coxall@mths.ca>
2021-09-21 22:36:11 -0400

    Update variables.js

--- code_examples/3-Structured_Problem_Solving/1-Variables/JavaScript/variables.js
+++ code_examples/3-Structured_Problem_Solving/1-Variables/JavaScript/variables.js
@@ -1,9 +1,15 @@
 /**
  * Created by: Mr. Coxall
- * Created on: June 2020
+ * Created on: Sep 2020
  * This program shows declaring variables
  */
 
-var isCurrent = true;
-var age = 32;
-var someWords = "Hello, World!";
+var isCurrent = true  // bool
+var age = 32  // int
+var area = 32.45  // float
+var someWords = "Hello, World!"  // string
+
+console.log(isCurrent)
+console.log(age)
+console.log(area)
+console.log(someWords)
