45be9fd25c878c2f2c656dd4c88678c50df4d60b
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2022-03-31 09:08:24 -0400

    Create scopeOfVariables.java

---
+++ code_examples/3-Structured_Problem_Solving/Scope_of_Variables/Java/scopeOfVariables.java
@@ -0,0 +1,45 @@
+// Copyright (c) Jonathan Pasco-Arnone All rights reserved.
+//
+// Created by: Jonathan Pasco-Arnone
+// Created on: Mar 2022
+// This program shows how local and global variables work
+
+class Main {
+
+    /**
+    * Global variable
+    */
+    private static int variableX = 25;
+
+    /**
+    * This shows what happens with local variables
+    */
+    public static void localVariable() {
+        int variableX = 10;
+        int variableY = 30;
+        int variableZ = variableX + variableY;
+        System.out.println("Local variableX, variableY, variableZ: "
+            + variableX + " + " + variableY + " = " + variableZ);
+        
+    }
+
+    /**
+    * This shows what happens with global variables
+    */
+    public static void globalVariable() {
+        variableX = variableX + 1;
+        int variableY = 30;
+        int variableZ = variableX + variableY;
+        System.out.println("Global variableX. Local variableY, variableZ: "
+            + variableX + " + " + variableY + " = " + variableZ);
+        
+    }
+
+    /**
+    * This function calls local and global
+    */
+    public static void main(String[] args) {
+        localVariable();
+        globalVariable();
+    }
+}
