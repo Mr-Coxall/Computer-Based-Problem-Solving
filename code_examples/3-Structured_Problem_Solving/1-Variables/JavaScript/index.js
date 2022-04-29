b4b5005ab75988a327e58c082a7d08a02f6c4196
Patrick Coxall <mr.coxall@mths.ca>
2022-04-29 10:49:52 -0400

    fix JS

--- code_examples/3-Structured_Problem_Solving/1-Variables/JavaScript/index.js
+++ code_examples/3-Structured_Problem_Solving/1-Variables/JavaScript/index.js
@@ -4,10 +4,11 @@
  * This program shows declaring variables
  */
 
-var isCurrent = true  // bool
-var age = 32  // int
-var area = 32.45  // float
-var someWords = "Hello, World!"  // string
+// variable definition
+var isCurrent = true // bool
+var age = 32 // int
+var area = 32.45 // float
+var someWords = "Hello, World!" // string
 
 console.log(isCurrent)
 console.log(age)
