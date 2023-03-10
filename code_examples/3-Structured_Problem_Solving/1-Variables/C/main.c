5b6acf5d39da805cd7fa6b1e074007b63eb419cc
Patrick Coxall <mr.coxall@mths.ca>
2023-03-10 09:59:41 -0500

    C update

---
+++ code_examples/3-Structured_Problem_Solving/1-Variables/C/main.c
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved.
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This program shows declaring variables
+
+variable definition
+
+int main() {
+    // variable definition
+    bool isCurrent = true;  // bool
+    int age = 32;  // int
+    float area = 32.45;  // float
+    char someWords[13] = "Hello, World!";  // string
+
+    printf("%d\n", isCurrent);
+    printf("%d\n", age);
+    printf("%.2\n", area);
+    printf("%s\n", someWords);
+
+    printf("\nDone.\n");
+    return 0;
+}
