17bc7364d28a4055307b13ac07476927fe5d6df2
Patrick Coxall <mr.coxall@mths.ca>
2023-03-20 20:39:29 +0000

    java lint fix

--- code_examples/3-Structured_Problem_Solving/9-Select-Case/Java/Main.java
+++ code_examples/3-Structured_Problem_Solving/9-Select-Case/Java/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     // this function checks a traffic light
-  
+
     // create Scanner object for user input
     Scanner scanner = new Scanner(System.in);
 
