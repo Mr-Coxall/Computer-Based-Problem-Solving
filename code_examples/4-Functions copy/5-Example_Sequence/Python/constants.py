7072845f0fc33a28e70710b2eef56f57e4bddd8e
Patrick Coxall <mr.coxall@mths.ca>
2023-04-11 21:45:18 -0400

    code update

--- code_examples/4-Functions copy/5-Example_Sequence/Python/constants.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-Created by: Mr. Coxall
-Created on: Sept 2020
-This module contains constants for the tax program
-"""
-
-HST = 0.13
--- code_examples/5-Holding_Data/14-Do_While_Loop/Go/main.c
+++
@@ -1,1 +0,0 @@
-// No Do ... While loop in Go
