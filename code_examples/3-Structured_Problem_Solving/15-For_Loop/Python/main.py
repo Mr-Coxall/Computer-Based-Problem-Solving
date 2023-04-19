7df0c3b5cdbb10dfdde1f0a68db39e2273499cbd
Patrick Coxall <mr.coxall@mths.ca>
2023-04-19 04:32:03 -0400

    omment fix

--- code_examples/3-Structured_Problem_Solving/15-For_Loop/Python/main.py
+++ code_examples/3-Structured_Problem_Solving/15-For_Loop/Python/main.py
@@ -2,12 +2,12 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module uses a while loop
+This module uses a for loop
 """
 
 
 def main() -> None:
-    """The main() this function uses a while loop, returns None."""
+    """The main() this function uses a for loop, returns None."""
 
     # input
     positive_integer = int(input("Enter how many times to repeat: "))
