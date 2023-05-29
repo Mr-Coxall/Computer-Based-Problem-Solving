111d3ba5dec9a6d59a5d2dcfad126c46d8e515a2
Patrick Coxall <mr.coxall@mths.ca>
2023-05-29 23:24:11 +0000

    update

--- code_examples/5-Holding_Data/1-Arrays/4-Array_2D/Go/main.go
+++ code_examples/5-Holding_Data/1-Arrays/4-Array_2D/Go/main.go
@@ -1,6 +1,54 @@
 /**
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program uses an array as a parameter
+ * This program uses a 2D array
  */
 
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func sumOfNumbers(arrayOfNumbers [][]int) int {
+	total := 0
+
+	for _, row := range arrayOfNumbers {
+		for _, value := range row {
+			total += value
+		}
+	}
+
+	return total
+}
+
+func main() {
+	var a2DList [][]int
+	var sumAnswer int
+
+	// input
+	var rows, columns int
+	fmt.Print("How many rows would you like: ")
+	fmt.Scanln(&rows)
+	fmt.Print("How many columns would you like: ")
+	fmt.Scanln(&columns)
+
+	for i := 0; i < rows; i++ {
+		tempColumn := make([]int, columns)
+		for j := 0; j < columns; j++ {
+			aRandomNumber := rand.Intn(10)
+			tempColumn[j] = aRandomNumber
+			fmt.Printf("%d ", aRandomNumber)
+		}
+		a2DList = append(a2DList, tempColumn)
+		fmt.Println()
+	}
+
+	sumAnswer = sumOfNumbers(a2DList)
+
+	// output
+	fmt.Printf("\nThe sum of all the numbers is: %d\n", sumAnswer)
+
+	fmt.Println("\nDone.")
+}
