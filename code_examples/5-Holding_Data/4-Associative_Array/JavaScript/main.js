f6b7ad4fe1a87c1ead0571e0b2e417cd4cf65575
Patrick Coxall <mr.coxall@mths.ca>
2023-05-31 18:24:17 +0000

    update

--- code_examples/5-Holding_Data/4-Associative_Array/JavaScript/main.js
+++ code_examples/5-Holding_Data/4-Associative_Array/JavaScript/main.js
@@ -1,25 +1,42 @@
 /* Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program uses a nested if statement
+ * This program uses a map
  */
 
 const prompt = require("prompt-sync")()
 
-// input
-const cookiesSoldStr = prompt("Enter the number of boxes of cookies you sold: ")
-console.log("")
+// this function uses a map
+let airportName;
 
-// process & output
-const cookiesSold = parseInt(cookiesSoldStr)
+// an empty map
+const airports = new Map();
 
-if (cookiesSold >= 20) {
-  if (cookiesSold < 30) {
-    console.log("You get a small prize.")
-  } else {
-    console.log("You get a large prize.")
-  }
+// a map filled with data
+const someInfo = new Map([
+  ['a', 1],
+  ['b', 2],
+  ['c', 3],
+]);
+
+// adding items
+airports.set("YYZ", "Toronto Pearson");
+airports.set("YOW", "Ottawa Canada");
+airports.set("DUB", "Dublin Ireland");
+airports.set("LHR", "London Heathrow");
+
+console.log("All the airports:");
+for (let [code, name] of airports) {
+  console.log(`The airport code is ${code} for ${name}`);
+}
+console.log();
+
+airportName = prompt("Type in an airport code: ");
+console.log();
+
+if (airports.has(airportName)) {
+  console.log("The name of the airport you chose is", airports.get(airportName));
 } else {
-  console.log("No prize.")
+  console.log("That airport is not in the airport's dictionary.");
 }
 
 console.log("\nDone.")
