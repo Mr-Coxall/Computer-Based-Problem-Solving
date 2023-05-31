20e503d089709c158df06b2953b5f10fe1cc2b6c
Patrick Coxall <mr.coxall@mths.ca>
2023-05-31 17:33:26 +0000

    java

--- code_examples/5-Holding_Data/4-Associative_Array/Java/Main.java
+++ code_examples/5-Holding_Data/4-Associative_Array/Java/Main.java
@@ -1,5 +1,5 @@
 /*
- * This program uses a nested if statement
+ * This program uses a uses a map
  *
  * @author  Mr Coxall
  * @version 1.0
@@ -7,30 +7,46 @@
  */
 
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
-    // this function uses a nested if statement
+    // this function uses a uses a map
 
-    // create Scanner object for user input
-    Scanner scanner = new Scanner(System.in);
+    // this function uses a map
+    String airportName;
 
-    // input
-    System.out.print("Enter the number of boxes of cookies you sold: ");
-    String cookiesSoldStr = scanner.nextLine();
+    // an empty map
+    Map<String, String> airports = new HashMap<>();
+
+    // a map filled with data
+    Map<Character, Integer> someInfo = new HashMap<>();
+    someInfo.put('a', 1);
+    someInfo.put('b', 2);
+    someInfo.put('c', 3);
+
+    // adding items
+    airports.put("YYZ", "Toronto Pearson");
+    airports.put("YOW", "Ottawa Canada");
+    airports.put("DUB", "Dublin Ireland");
+    airports.put("LHR", "London Heathrow");
+
+    System.out.println("All the airports:");
+    for (Map.Entry<String, String> entry : airports.entrySet()) {
+        System.out.println("The airport code is " + entry.getKey() + " for " + entry.getValue());
+    }
     System.out.println();
 
-    // process & output
-    int cookiesSold = Integer.parseInt(cookiesSoldStr);
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Type in an airport code: ");
+    airportName = scanner.nextLine();
+    System.out.println();
 
-    if (cookiesSold >= 20) {
-      if (cookiesSold < 30) {
-        System.out.println("You get a small prize.");
-      } else {
-        System.out.println("You get a large prize.");
-      }
+    if (airports.containsKey(airportName)) {
+        System.out.println("The name of the airport you chose is " + airports.get(airportName));
     } else {
-      System.out.println("No prize.");
+        System.out.println("That airport is not in the airport's dictionary.");
     }
 
     // close the Scanner object
