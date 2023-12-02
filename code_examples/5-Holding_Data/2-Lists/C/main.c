7ba2ff54a847aba0b73ec315e4a9b442ac1e4d28
Patrick Coxall <mr.coxall@mths.ca>
2023-12-02 19:57:01 +0000

    C dynamic arrays

--- code_examples/5-Holding_Data/2-Lists/C/main.c
+++ code_examples/5-Holding_Data/2-Lists/C/main.c
@@ -1,1 +1,3 @@
-// No list or Linked List standard in C
+// No list or Linked List standard in C.
+// You can create dynamic arrays by using maloc(), but that is beyond the scope of this book.
+// https://www.geeksforgeeks.org/dynamic-array-in-c/
